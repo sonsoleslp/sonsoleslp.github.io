a36fe571aa6841558c76aa67eae71121da1f82fa
sonsoleslp <sonsoleslp@hotmail.com>
2019-01-19 00:39:14 +0100

    QUIZ Converter

---
+++ quiz-converter/precache-manifest.1a3d36a2d8f419d74b80574a4b95ff26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "93d3943525adf9cbdcb1",
+    "url": "./static/js/main.93d39435.chunk.js"
+  },
+  {
+    "revision": "6d7d7719a12218826fee",
+    "url": "./static/js/1.6d7d7719.chunk.js"
+  },
+  {
+    "revision": "93d3943525adf9cbdcb1",
+    "url": "./static/css/main.4662a9cf.chunk.css"
+  },
+  {
+    "revision": "0a94d802979542bd473adf2a4b398d37",
+    "url": "./index.html"
+  }
+];
---
+++ quiz-converter/precache-manifest.aa86363d8b39c3d4382af1958a196e77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4b90ecc8a4158e5319b2",
+    "url": "./static/js/main.4b90ecc8.chunk.js"
+  },
+  {
+    "revision": "6d7d7719a12218826fee",
+    "url": "./static/js/1.6d7d7719.chunk.js"
+  },
+  {
+    "revision": "4b90ecc8a4158e5319b2",
+    "url": "./static/css/main.4662a9cf.chunk.css"
+  },
+  {
+    "revision": "c4d208c13f1160373a5086c3b57bd89f",
+    "url": "./index.html"
+  }
+];
