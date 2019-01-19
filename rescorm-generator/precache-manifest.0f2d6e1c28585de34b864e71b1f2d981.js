a90e743c39a27e89ecb72e2de3f18fee11be00c5
sonsoleslp <sonsoleslp@hotmail.com>
2019-01-19 19:17:09 +0100

    RESCORM Generator

---
+++ rescorm-generator/precache-manifest.0f2d6e1c28585de34b864e71b1f2d981.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "dfa1bbc1c3418811f762",
+    "url": "./static/js/main.dfa1bbc1.chunk.js"
+  },
+  {
+    "revision": "0af004d3c497a92c9982",
+    "url": "./static/js/1.0af004d3.chunk.js"
+  },
+  {
+    "revision": "dfa1bbc1c3418811f762",
+    "url": "./static/css/main.e5c6f10c.chunk.css"
+  },
+  {
+    "revision": "b21ea739ec43844021c0c366c08c0a65",
+    "url": "./index.html"
+  }
+];
